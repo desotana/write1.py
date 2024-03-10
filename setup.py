904c2258db81f094261f8f2d1e7e08fc496b98f9
xyz <desotanalomolatachi@gmail.com>
2024-03-11 01:32:39 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name='write',
-    version='2020.12.3',
+    name='write1',
+    version='2024.03.01',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
